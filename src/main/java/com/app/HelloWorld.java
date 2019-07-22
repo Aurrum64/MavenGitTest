ed3b4e6e388a64936caefc7649a19b9cabcf4e84
Artem Lebedev <Aurrum64@gmail.com>
2019-07-22 15:53:39 +0300

    Add secong string in my program

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -2,6 +2,7 @@ package com.app;
 
 public class HelloWorld {
     public static void main(String[] argv) {
-        System.out.println("Hello, world!);
+        System.out.println("Hello, world!");
+		System.out.println("Second string in my program.");
     }
 }
