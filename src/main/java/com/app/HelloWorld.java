e8d41994f7edd5ef4c5a0c6055b1931a34146a62
Artem Lebedev <Aurrum64@gmail.com>
2019-07-22 16:03:25 +0300

    Add arithmetic operation

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
     public static void main(String[] argv) {
         System.out.println("Hello, world!");
 		System.out.println("Second string in my program.");
+		int a = 10;
+		int b = 5;
+		int c = a + b;
+		System.out.println(c);
     }
 }
